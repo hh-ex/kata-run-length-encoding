189bc252a7049283ff0571ed777c25d5b4dc13b5
Alexander Schramm <alex@schram.me>
2019-01-29 21:07:35 +0100

    stuff

--- lib/run_length_encoding.ex
+++ lib/run_length_encoding.ex
@@ -9,8 +9,27 @@ defmodule RunLengthEncoding do
   require Logger
 
   def encode(string) do
+    string
+    |> String.codepoints()
+    |> Stream.chunk_by(& &1)
+    |> Stream.map(&map_chunk/1)
+    |> Enum.join()
   end
 
+  defp map_chunk([letter]), do: letter
+  defp map_chunk([letter | _rest] = arr), do: "#{length(arr)}#{letter}"
+
   def decode(string) do
+    string
+    |> String.codepoints()
+    |> Enum.reduce({[], nil}, fn char, {str, times} ->
+      case Integer.parse(char) do
+        :error -> {[String.duplicate(char, times || 1) | str], nil}
+        {count, _} -> {str, if(times, do: times * 10 + count, else: count)}
+      end
+    end)
+    |> elem(0)
+    |> Enum.reverse()
+    |> Enum.join()
   end
 end
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule RunLengthEncoding.MixProject do
     [
       app: :run_length_encoding,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
